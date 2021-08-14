0f5e734d9bf67304e444abddf16fec9dd970e165
adigaandyt <69291901+adigaandyt@users.noreply.github.com>
2021-08-14 19:40:40 +0300

    updated files

--- HETS_PROJECT/HETS_PROJECT/src/LogIn.java
+++ HETS_PROJECT/HETS_PROJECT/src/LogIn.java
@@ -8,11 +8,20 @@ import javax.swing.JOptionPane;
 
 import java.awt.Button;
 import javax.swing.JTextField;
+import javax.swing.text.Document;
+
 import java.awt.Font;
 import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
+import com.mongodb.*;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
 
+import java.util.Arrays;
+import javax.swing.JOptionPane;
 public class LogIn {
 
 	private JFrame frmLogIn;
@@ -50,27 +59,65 @@ public class LogIn {
 		frmLogIn.setBounds(100, 100, 450, 300);
 		frmLogIn.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmLogIn.getContentPane().setLayout(null);
-		
+		JButton btnAddusr = new JButton("New User");
+
 		JButton btnLogin = new JButton("log in");
 		/*button login onclick*/
 		btnLogin.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent e)
 			{
-				if(txtId.getText().equals("rash")&&txtPassword.getText().equals("123")) {
-				Home home=new Home();
-				Home.main(null); 
-            	frmLogIn.dispose();		
+				try {
+				
+					MongoClient mng = new MongoClient("localhost",27017);
+					DB db = mng.getDB("test");
+				
+					System.out.println("connected!!!");
+					DBCollection usercoll = db.getCollection("users");
+					BasicDBObject d1 = new BasicDBObject("name",txtId.getText()).append("password", txtPassword.getText());
+				
+					if((usercoll.find(d1).next()!=null)) {
+						System.out.println((usercoll.find(d1).next()));
+						Home home=new Home();
+						Home.main(null); 
+		            	frmLogIn.dispose();	
+					}
+					
+					System.out.println("**********");
+					System.out.println("-----");
+			
+					mng.close();
+					
 				}
-				else
+				catch(Exception e1)
 				{
 					JOptionPane.showMessageDialog(null, "The ID or password is incorrect");
 				}
+				
+				
 			}
 		}
 		);
+		btnAddusr.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				try {
+					NewUser newusr=new NewUser();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				NewUser.main(null); 
+            	frmLogIn.dispose();			
+			}
+		}
+		);
+
 		btnLogin.setBounds(213, 190, 68, 23);
 		frmLogIn.getContentPane().add(btnLogin);
+		
+		btnAddusr.setBounds(300, 220, 100, 23);
+		frmLogIn.getContentPane().add(btnAddusr);
 		
 		txtId = new JTextField();
 		txtId.setBounds(172, 90, 155, 20);
@@ -81,17 +128,18 @@ public class LogIn {
 		lblId.setBounds(76, 93, 74, 14);
 		frmLogIn.getContentPane().add(lblId);
 		
-		JLabel lblLogIn = new JLabel("Log in");
+		JLabel lblLogIn = new JLabel("Log In");
 		lblLogIn.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblLogIn.setBounds(172, 30, 68, 28);
 		frmLogIn.getContentPane().add(lblLogIn);
 		
-		JLabel lblPassword = new JLabel("password:");
+		JLabel lblPassword = new JLabel("Password:");
 		lblPassword.setBounds(76, 134, 74, 14);
 		frmLogIn.getContentPane().add(lblPassword);
 		
 		txtPassword = new JPasswordField();
 		txtPassword.setBounds(172, 131, 155, 20);
 		frmLogIn.getContentPane().add(txtPassword);
+
 	}
 }
